abc33368c9da4bf32aa9a82f2da33022aea98b4f
Maxim <m.skibitsky@cometrica.ru>
2020-09-16 15:27:35 +0300

    Добавил тесты tsd

--- src/components/Operation.tsx
+++ src/components/Operation.tsx
@@ -7,34 +7,34 @@ import {
 
 function Operation({operation, title}: { operation: Function, title: string }) {
     const [nums, setNums] = useState(['0', '0']);
-        const cleanNums: Array<number> = nums.map((num: string) => {
-            const res = parseInt(num);
-            return isNaN(res) ? 0 : res;
+    const cleanNums: Array<number> = nums.map((num: string) => {
+        const res = parseInt(num);
+        return isNaN(res) ? 0 : res;
+    });
+    const result: number = operation(cleanNums);
+
+    const onChangeHandler = useCallback(({ target }) => {
+        const { index }: { index: number } = target.dataset;
+        const { value }: {value: string} = target;
+        
+        setNums((state) => {
+            const res: Array<string> = [...state];
+            res[index] = value;
+            return res;
         });
-        const result: number = operation(cleanNums);
+    }, []);
 
-        const onChangeHandler = useCallback(({ target }) => {
-            const { index }: { index: number } = target.dataset;
-            const { value }: {value: string} = target;
-            
-            setNums((state) => {
-                const res: Array<string> = [...state];
-                res[index] = value;
-                return res;
-            });
-        }, []);
-
-        return (
-            <StyledOperation onChange={onChangeHandler}>
-                <h2>{title}</h2>
-                <div>
-                    { nums.map((value, index) => <StyledUserInput data-index={index} value={value}/>) }
-                </div>
-                <StyledResult>
-                    Ответ: { isNaN(result) ? '--' : result }
-                </StyledResult>
-            </StyledOperation>
-        )
+    return (
+        <StyledOperation onChange={onChangeHandler}>
+            <h2>{title}</h2>
+            <div>
+                { nums.map((value, index) => <StyledUserInput data-index={index} value={value}/>) }
+            </div>
+            <StyledResult>
+                Ответ: { isNaN(result) ? '--' : result }
+            </StyledResult>
+        </StyledOperation>
+    )
 }
 
 export default React.memo(Operation);
--- test/helpers.test.ts
+++
@@ -1,9 +0,0 @@
-const { multiply, sum } = require('../src/helpers.ts');
-
-test('Multiple 4 and 5 equals 20', () => {
-    expect(multiply([4, 5])).toBe(20);
-});
-
-test('Sum 10 and 22 equal 32', () => {
-    expect(sum([10, 22])).toBe(32);
-})
